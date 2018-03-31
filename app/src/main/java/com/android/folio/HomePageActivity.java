56e845d484d2c5c380eb880d10d4068635ec681a
Erick Suarez <mr.ericksuarez@gmail.com>
2018-03-31 16:11:46 -0700

    Add analyzeText

--- app/src/main/java/com/android/folio/HomePageActivity.java
+++ app/src/main/java/com/android/folio/HomePageActivity.java
@@ -1,5 +1,7 @@
 package com.android.folio;
 
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +9,20 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.services.language.v1beta2.CloudNaturalLanguage;
+import com.google.api.services.language.v1beta2.CloudNaturalLanguageRequestInitializer;
+import com.google.api.services.language.v1beta2.model.AnnotateTextRequest;
+import com.google.api.services.language.v1beta2.model.AnnotateTextResponse;
+import com.google.api.services.language.v1beta2.model.Document;
+import com.google.api.services.language.v1beta2.model.Features;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
 
 public class HomePageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,8 +30,7 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
         // Declare Variables
         //==============================================================================================
         private FirebaseAuth mAuth;
-        private Button buttonSignOut;
-        ImageView imageView;
+        private String CLOUD_API_KEY = "AIzaSyBP_3jPRzVum-DnQqie68laZ3dWGgNaHow";
 
         //==============================================================================================
         // On Create Setup
@@ -39,7 +51,7 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
             setContentView(R.layout.activity_home_page);
 
             //Add ActionListeners
-
+            findViewById(R.id.analyze_button).setOnClickListener(this);
             findViewById(R.id.buttonSignOut).setOnClickListener(this);
         }
 
@@ -56,7 +68,62 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
                     startActivity(new Intent(this, MainActivity.class));
                     break;
 
+                case R.id.analyze_button:
+                    analyzeText("Hello World");
+
             }
+        }
+
+        public void analyzeText(String transcript) {
+            final CloudNaturalLanguage naturalLanguageService =
+                    new CloudNaturalLanguage.Builder(
+                            AndroidHttp.newCompatibleTransport(),
+                            new AndroidJsonFactory(),
+                            null
+                    ).setCloudNaturalLanguageRequestInitializer(
+                            new CloudNaturalLanguageRequestInitializer(CLOUD_API_KEY)
+                    ).build();
+
+            Document document = new Document();
+            document.setType("PLAIN_TEXT");
+            document.setLanguage("en-US");
+            document.setContent(transcript);
+
+            Features features = new Features();
+            features.setExtractDocumentSentiment(true);
+
+            final AnnotateTextRequest request = new AnnotateTextRequest();
+            request.setDocument(document);
+            request.setFeatures(features);
+
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    AnnotateTextResponse response =
+                            null;
+                    try {
+                        response = naturalLanguageService.documents()
+                                .annotateText(request).execute();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    final float sentiment =((response.getDocumentSentiment().getScore())+1)*5;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            
+                            AlertDialog dialog =
+                                    new AlertDialog.Builder(HomePageActivity.this)
+                                            .setTitle("Sentiment: " + sentiment)
+                                            .setNeutralButton("Okay", null)
+                                            .create();
+                            dialog.show();
+                        }
+                    });
+                }
+            });
         }
 
     }
